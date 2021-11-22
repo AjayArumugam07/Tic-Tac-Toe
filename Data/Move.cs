910f24581ecd2a97a3e04ff1c16ab3650732a1fc
Ajay Arumugam <ajayarumugam07@gmail.com>
2021-11-21 23:08:02 -0700

    Entities Created

---
+++ Data/Move.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Data
+{
+    public class Move {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+    }
+}
